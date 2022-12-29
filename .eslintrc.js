4d0a547931b33f530b80003e622f35c4cef0a0ae
krisnaputra31 <81416312+krisna31@users.noreply.github.com>
2022-12-29 17:34:31 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,15 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: "airbnb-base",
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
   },
   rules: {
+    "no-shadow": "off",
+    "linebreak-style": "off",
+    quotes: "off",
+    "no-underscore-dangle": "off",
   },
 };
