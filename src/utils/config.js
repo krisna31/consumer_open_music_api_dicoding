2ecdef2bf18f3010f253bb7700d3c22980e8ffb4
krisnaputra31 <81416312+krisna31@users.noreply.github.com>
2022-12-29 14:10:08 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,10 @@
+/* eslint-disable linebreak-style */
 const config = {
   nodemailer: {
-    smtpHost: process.env.SMTP_HOST,
-    smtpPort: process.env.SMTP_PORT,
-    smtpUser: process.env.SMTP_USER,
-    smtpPassword: process.env.SMTP_PASSWORD,
+    smtpHost: process.env.MAIL_HOST,
+    smtpPort: process.env.MAIL_PORT,
+    smtpUser: process.env.MAIL_ADDRESS,
+    smtpPassword: process.env.MAIL_PASSWORD,
   },
   rabbitMq: {
     server: process.env.RABBITMQ_SERVER,
